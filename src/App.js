0c103f0c217ce10d3c960efbf5f876e7df49505b
Patrick Gorospe <pgorospe@Patricks-MacBook-Air.local>
2020-05-06 11:26:47 -0400

    Adding API call to test integration of services

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const serviceUrl =
+    process.env.UNDERCUT_SERVICE_URL || "http://localhost:3000/hello";
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = async () => {
+    console.log(`Making request at ${serviceUrl}`);
+    const response = await fetch(serviceUrl);
+    const apiData = await response.json();
+    console.log(apiData);
+    setData(apiData.message);
+    console.log(`Received data from our api: ${JSON.stringify(apiData)}`);
+    return apiData;
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to Undercut 💇</p>
+        <p>
+          Welcome to Undercut <span role="img">💇</span>
+        </p>
+        <p>
+          Received data from our api: <strong>{data}</strong>
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
