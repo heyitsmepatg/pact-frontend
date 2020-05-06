101b394ae56edac2b089eeb37dd1ebbc14837847
Patrick Gorospe <pgorospe@Patricks-MacBook-Air.local>
2020-05-06 17:46:31 -0400

    Add form post

--- src/App.js
+++ src/App.js
@@ -4,21 +4,40 @@ import "./App.css";
 
 function App() {
   const serviceUrl =
-    process.env.UNDERCUT_SERVICE_URL || "http://localhost:3000/";
+    process.env.UNDERCUT_SERVICE_URL || "http://localhost:3000";
   const [data, setData] = useState([]);
+
+  const [message, setMessage] = useState([]);
 
   useEffect(() => {
     loadData();
   }, []);
 
   const loadData = async () => {
-    console.log(`Making request at ${serviceUrl}`);
+    console.log(`Making get request at ${serviceUrl}`);
     const response = await fetch(serviceUrl);
     const apiData = await response.json();
     console.log(apiData);
     setData(apiData.message);
     console.log(`Received data from our api: ${JSON.stringify(apiData)}`);
     return apiData;
+  };
+
+  const handleMessageSubmit = async (event) => {
+    event.preventDefault();
+    const postUrl = `${serviceUrl}/message`;
+    const response = await fetch(postUrl, {
+      method: "post",
+      body: event.target.value,
+    });
+    console.log(
+      `Response from POST request is: ${JSON.stringify(response.JSON)}`
+    );
+  };
+
+  const handleMessageChange = (event) => {
+    console.log(`value be: ${event.target.value}`);
+    setMessage(event.target.value);
   };
   return (
     <div className="App">
@@ -28,12 +47,13 @@ function App() {
           Welcome to Undercut <span role="img">💇</span>
         </p>
 
-        <form>
-            <label>
-                Location: 
-                <input type="text" location="location" />
-            </label>
-            <input type="submit" value="Submit" />
+        <form onSubmit={handleMessageSubmit}>
+          <label>
+            Message:
+            <input type="text" onChange={handleMessageChange} />
+          </label>
+          <p>Your Message is: {message}</p>
+          <input type="submit" value="Submit" />
         </form>
 
         <p>
